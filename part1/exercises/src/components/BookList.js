76f7806ebde6beee41e982679a209c399c1cdd07
Austin <kenjigw@gmail.com>
2023-08-03 16:46:34 -0500

    updated books chores list

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Radical Reads";
+   let book1 = "https://www.topshelfcomix.com/catalog/covers/they-called-us-enemy-cover-150dpi_lg.jpg";
+   let book2 = "https://cdn.imagecomics.com/assets/i/releases/984996/magic-order-4-6-of-6_975a33fffc.jpg";
+   let book3 = "https://cdn.imagecomics.com/assets/i/releases/999526/plush-tp-mr_dafeb5dd3e.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="They Called Us Enemy" />
+         <img src={book2} alt="Magic Order" />
+         <img src={book3} alt="Plush" />
       </div>      
    );
 }
