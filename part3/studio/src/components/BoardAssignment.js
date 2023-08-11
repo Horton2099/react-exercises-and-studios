8ac8c3ae6ce18e7ddaaac5e3cfa3466f8dda4293
Austin <kenjigw@gmail.com>
2023-08-10 19:45:54 -0500

    updated boardjs

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [{label: "Food Truck"}, {label: "Hispanic Classics"}, {label: "Festival Foods"}];
+
+   const [boardName, setBoardName] = useState("no boards yet!");
 
    const handleChange = (event) => {
+         setBoardName(event.target.value);
+         // Check Later
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+           <option value={board.value} key={board.value}>{board.label}</option> 
+         ))}
       </select>
 
       <p>Saved to {boardName}!</p>
